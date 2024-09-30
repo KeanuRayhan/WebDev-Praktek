648a5a041eae6a2c63124058cc390cd0df8573c6
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-09-30 10:56:13 +0700

    Create MovieCard.jsx

---
+++ web-movie/src/components/MovieCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MovieCard = ({ title, url, year, genres }) => {
+  return (
+    <div className="bg-gray-800 border border-gray-700 shadow rounded-lg p-4">
+      <img src={url} alt={title} className="w-full h-56 object-cover mb-2" />
+      <h2 className="text-base leading-5 font-bold mb-2 text-white">
+        {title}
+      </h2>
+      <p className="text-sm">{year}</p>
+      <p className="text-sm">{genres.join(', ')}</p>
+      {/* <p className="text-sm">Rate {rating}/5</p>
+      <p className="text-sm">{views} views</p> */}
+    </div>
+  );
+};
+
+export default MovieCard;
