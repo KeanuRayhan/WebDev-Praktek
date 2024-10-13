6d0a5ff367efafaffc52dd32e5924e8c21c192e5
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-10-13 20:14:31 +0700

    Update MovieCard.jsx

--- web-movie/src/components/MovieCard.jsx
+++ web-movie/src/components/MovieCard.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MovieCard = ({ title, url, year, genres }) => {
+const MovieCard = ({ title, url, year, genres, id }) => {
   return (
-    <div className="bg-gray-800 border border-gray-700 shadow rounded-lg p-4">
-      <img src={url} alt={title} className="w-full h-56 object-cover mb-2" />
-      <h2 className="text-base leading-5 font-bold mb-2 text-white">
-        {title}
-      </h2>
-      <p className="text-sm">{year}</p>
-      <p className="text-sm">{genres.join(', ')}</p>
-      {/* <p className="text-sm">Rate {rating}/5</p>
-      <p className="text-sm">{views} views</p> */}
-    </div>
+    <Link to={`/movies/${id}`}>
+      <div className="bg-gray-800 border border-gray-700 shadow rounded-lg p-4">
+        <img src={url} alt={title} className="w-full h-56 object-cover mb-2" />
+        <h2 className="text-base leading-5 font-bold mb-2 text-white">
+          {title}
+        </h2>
+        <p className="text-sm">{year}</p>
+        <p className="text-sm">{genres.join(', ')}</p>
+        {/* <p className="text-sm">Rate {rating}/5</p>
+        <p className="text-sm">{views} views</p> */}
+      </div>
+    </Link>
   );
 };
 
