e82140b99995f700db3e55491ae3162726f56a5c
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-10-13 17:41:39 +0700

    Update TrailerSection.jsx

--- web-movie/src/components/TrailerSection.jsx
+++ web-movie/src/components/TrailerSection.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
-const TrailerSection = () => {
+const TrailerSection = ({ videoUrl }) => {
+    const getEmbedUrl = (url) => {
+        if (!url) return '';
+        
+        const urlParts = new URL(url);
+        const videoId = urlParts.searchParams.get('v');
+        if (!videoId) return ''; 
+        
+        return `https://www.youtube.com/embed/${videoId}`;
+    };
+
+    const embedUrl = getEmbedUrl(videoUrl);
+    console.log('Embed URL:', embedUrl);
+
     return (
         <div className="w-auto mx-auto mt-2">
             <div className="mt-4">
                 <iframe
                     width="100%"
                     height="480"
-                    src="https://www.youtube.com/embed/example-trailer"
+                    src={embedUrl}
                     title="Trailer"
                     frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                     className="rounded border border-gray-700"
                 ></iframe>
@@ -19,4 +33,3 @@ const TrailerSection = () => {
 };
 
 export default TrailerSection;
-
