5a5e73526970be79cc64a36faeea49b27b7c6c61
KeanuRayhan <117803802+KeanuRayhan@users.noreply.github.com>
2024-10-14 00:26:39 +0700

    Update server.js

--- backend-movie/server.js
+++ backend-movie/server.js
@@ -35,6 +35,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/movie.routes")(app);
 require("./app/routes/genre.routes")(app);
+require("./app/routes/plaform.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
