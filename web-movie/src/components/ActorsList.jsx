050ef6823a474c7713b94702bc2a7f4d1cad964a
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-10-13 17:46:10 +0700

    Update ActorsList.jsx

--- web-movie/src/components/ActorsList.jsx
+++ web-movie/src/components/ActorsList.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
-const ActorsList = () => {
+const ActorsList = ({ actors }) => {
     return (
         <div className="mt-8 grid grid-cols-10 gap-1">
-            {/* Ulangi ini untuk setiap aktor */}
-            <div className="text-center">
-                <img src="actor1.png" alt="Actor 1" className="w-24 h-36 mx-auto rounded bg-gray-700 object-cover" />
-                <p className="mt-2">Actor Name</p>
-            </div>
-            {/* Tambahkan aktor lain di sini */}
+            {actors.map((actor, index) => {
+                return (
+                    <div key={index} className="text-center">
+                        <img 
+                            src={actor.url_photo} 
+                            alt={actor.actor_name} 
+                            className="w-24 h-36 mx-auto rounded bg-gray-700 object-cover" 
+                        />
+                        <p className="mt-2">{actor.actor_name}</p>
+                    </div>
+                )
+            })}
         </div>
     );
 };
