6c4f597685d8d994a6d99fc516458ddc02098082
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-10-20 13:11:03 +0700

    Update MovieCard.jsx

--- web-movie/src/components/MovieCard.jsx
+++ web-movie/src/components/MovieCard.jsx
@@ -4,9 +4,15 @@ import { Link } from 'react-router-dom';
 const MovieCard = ({ title, url, year, genres, id }) => {
   return (
     <Link to={`/movies/${id}`}>
-      <div className="bg-gray-800 border border-gray-700 shadow rounded-lg p-4">
-        <img src={url} alt={title} className="w-full h-56 object-cover mb-2" />
-        <h2 className="text-base leading-5 font-bold mb-2 text-white">
+      <div className="bg-gray-800 border border-gray-700 shadow rounded-lg p-4 transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:border-yellow-500 hover:bg-gray-700 group">
+        <div className="relative overflow-hidden rounded-lg mb-2">
+          <img 
+            src={url} 
+            alt={title} 
+            className="w-full h-56 object-cover transition-transform duration-500 group-hover:scale-110" 
+          />
+        </div>
+        <h2 className="text-base leading-5 font-bold mb-2 text-white transition-colors duration-500 group-hover:text-yellow-400">
           {title}
         </h2>
         <p className="text-sm">{year}</p>
