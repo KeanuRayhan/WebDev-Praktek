3636aec8b1149e5a451c0180f0d16bbf5276135f
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-10-20 12:35:51 +0700

    Update user.model.js

--- backend-movie/app/models/user.model.js
+++ backend-movie/app/models/user.model.js
@@ -1,28 +1,27 @@
-module.exports = (sequelize, Sequelize) => {
-  const User = sequelize.define("Users", {
-    user_id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: Sequelize.STRING
-    },
-    email: {
-      type: Sequelize.STRING
-    },
-    password: {
-      type: Sequelize.STRING
-    },
-    role: {  // Menambahkan kolom role
-      type: Sequelize.ENUM('USER', 'ADMIN'),  // ENUM dengan nilai USER dan ADMIN
-      allowNull: false
-    }
-  }, {
-    freezeTableName: true,
-    timestamps: false
-  });
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("Users", {
+        user_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+        },
+        email: {
+            type: DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.STRING
+        },
+        role: {  // Menambahkan kolom role
+            type: DataTypes.ENUM('user', 'admin'),  // ENUM dengan nilai USER dan ADMIN
+            allowNull: false
+        },
+    }, {
+        freezeTableName: true,
+        timestamps: false
+    });
 
-  return User;
+    return User;
 };
-
