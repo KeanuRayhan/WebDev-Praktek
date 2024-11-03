e6f337d256b562a9053cb829c03d4743547d1070
Revandi2245 <revandi.faudiamar.tif422@polban.ac.id>
2024-11-03 21:15:25 +0700

    LOGIN_GOOGLE
    TES

---
+++ GOOGLE_AUTH/user.model.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("Users", {
+        user_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        google_id: {
+            type: DataTypes.STRING,
+        },
+        username: {
+            type: DataTypes.STRING,
+        },
+        email: {
+            type: DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.STRING
+        },
+        role: {  // Menambahkan kolom role
+            type: DataTypes.ENUM('user', 'admin'),  // ENUM dengan nilai USER dan ADMIN
+            allowNull: false
+        },
+    }, {
+        freezeTableName: true,
+        timestamps: false
+    });
+
+    return User;
+};
