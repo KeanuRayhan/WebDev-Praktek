9610148f7d95ac0b163b4e373ac55c9ba3131015
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-11-04 13:17:35 +0700

    Update src/services frontend

--- web-movie/src/services/auth.service.jsx
+++ web-movie/src/services/auth.service.jsx
@@ -24,6 +24,18 @@ const login = (username, password) => {
     });
 };
 
+const googleLogin = (token) => {
+    return axios.post(API_URL + "google-login", {
+        token,
+    }).then((response) => {
+        if (response.data.accessToken) {
+            localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
+        return response.data;
+    });
+};
+
 const logout = () => {
     localStorage.removeItem("user");
 };
@@ -32,11 +44,26 @@ const getCurrentUser = () => {
     return JSON.parse(localStorage.getItem("user"));
 };
 
+// Fungsi untuk mendapatkan user_id secara langsung
+const getUserId = () => {
+    const user = getCurrentUser();
+    return user ? user.id : null;
+};
+
+// Fungsi untuk mendapatkan accessToken secara langsung
+const getToken = () => {
+    const user = getCurrentUser();
+    return user ? user.accessToken : null;
+};
+
 const AuthService = {
     register,
     login,
     logout,
     getCurrentUser,
+    googleLogin,
+    getUserId,
+    getToken,
 };
 
 export default AuthService;
--- web-movie/src/services/movie.service.jsx
+++ web-movie/src/services/movie.service.jsx
@@ -1,4 +1,5 @@
 import http from "../http-common";
+import AuthService from "./auth.service";
 
 class MovieDataService {
     getAll() {
@@ -11,6 +12,113 @@ class MovieDataService {
 
     get(id) {
         return http.get(`/movies/${id}`);
+    };
+
+    createMovie(movieData) {
+        return http.post("/movies", movieData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            }
+        });
+    };
+
+    /* COUNTRY SERVICE */
+    // Mendapatkan semua country
+    getAllCountries() {
+        return http.get("/countries");
+    };
+
+    /* REVIEW SERVICE */
+    // Menambah review
+    addReview(data) {
+        const token = AuthService.getToken();
+        
+        return http.post("/reviews/add", data, {
+            headers: {
+                "x-access-token": token,
+            },
+        });
+    };
+
+    // Mendapatkan semua review dari seluruh movie
+    getAllReviews() {
+        return http.get("/reviews");
+    };
+
+    // Menghapus review
+    deleteReview(reviewId) {
+        return http.delete(`/reviews/${reviewId}`);
+    };
+
+    /* USER SERVICE */
+    // Mendapatkan semua user
+    getAllUsers() {
+        const token = AuthService.getToken();
+
+        return http.get("/users", {
+            headers: {
+                "x-access-token": token,
+            },
+        });
+    };
+
+    suspendUser(userId) {
+        return http.put(`/users/suspend/${userId}`);
+    };
+
+    /* GENRES SERVICE */
+    getAllGenres() {
+        return http.get("genres");
+    };
+
+    // Menambah genre
+    createGenre(data) {
+        const token = AuthService.getToken();
+
+        return http.post("/genres/add", data, {
+            headers: {
+                "x-access-token": token,
+            },
+        });
+    };
+
+    // Melakukan update genre
+    updateGenre(genreId, genreData) {
+        return http.put(`/genres/${genreId}`, genreData);
+    };
+
+    // Menghapus genre
+    deleteGenre(genreId) {
+        return http.delete(`/genres/${genreId}`);
+    };
+
+    /* ACTOR SERVICE */
+    // Mendapatkan semua actor
+    getAllActors() {
+        return http.get("/actors");
+    };
+
+    // Menambah actor
+    createActor(data) {
+        return http.post("/actors/add", data, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+    };
+
+    // Melakukan update actor
+    updateActor(actorId, actorData) {
+        return http.put(`/actors/${actorId}`, actorData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+    };
+
+    // Menghapus actor
+    deleteActor(actorId) {
+        return http.delete(`/actors/${actorId}`);
     };
 }
 
