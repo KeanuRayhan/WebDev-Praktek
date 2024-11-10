5ceeb236f06f846a31e4c8133473f4a5313fbd5f
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-11-10 17:57:31 +0700

    Update InputMovieForm.jsx

--- web-movie/src/components/InputMovieForm.jsx
+++ web-movie/src/components/InputMovieForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import http from "../http-common";
 import MovieDataService from '../services/movie.service';
 
@@ -7,13 +6,13 @@ const InputMovieForm = () => {
   const [formData, setFormData] = useState({
     title: '',
     year: '',
-    country: '',
+    country_id: '',
     synopsis: '',
     platforms: [],
     genres: [],
     actors: [],
     link_trailer: '',
-    award: '',
+    awardIds: '',
     url_photo: null
   });
 
@@ -24,6 +23,8 @@ const InputMovieForm = () => {
     countries: [],
     awards: []
   });
+
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchOptions = async () => {
@@ -36,12 +37,15 @@ const InputMovieForm = () => {
           http.get('/awards')
         ]);
 
+        // Filter awards yang memiliki movie_id null
+        const availableAwards = awardsRes.data.filter(award => award.movie_id === null);
+
         setOptions({
           actors: actorsRes.data,
           genres: genresRes.data,
           platforms: platformRes.data,
           countries: countriesRes.data,
-          awards: awardsRes.data
+          awards: availableAwards
         });
       } catch (error) {
         console.error('Error fetching options:', error);
@@ -58,40 +62,31 @@ const InputMovieForm = () => {
 
   const handleFileChange = (e) => {
     setFormData({ ...formData, url_photo: e.target.files[0] });
-  }
+  };
 
-  const handleGenreChange = (genre) => {
+  const handleActorChange = (actorId) => {
     setFormData((prevData) => {
-      const genres = prevData.genres.includes(genre)
-        ? prevData.genres.filter(g => g !== genre)
-        : [...prevData.genres, genre];
-      return { ...prevData, genres };
+      const isSelected = prevData.actors.includes(actorId);
+      const updatedActors = isSelected
+        ? prevData.actors.filter(id => id !== actorId)
+        : [...prevData.actors, actorId].slice(0, 9);
+      return { ...prevData, actors: updatedActors };
     });
   };
 
-  const handlePlatformChange = (platform) => {
-    setFormData((prevData) => {
-      const platforms = prevData.platforms.includes(platform)
-        ? prevData.platforms.filter(p => p !== platform)
-        : [...prevData.platforms, platform];
-      return { ...prevData, platforms };
-    });
+  const handleSearchActorChange = (e) => {
+    setSearchTerm(e.target.value);
   };
 
-  const handleActorChange = (actor) => {
-    setFormData((prevData) => {
-      const actors = prevData.actors.includes(actor)
-        ? prevData.actors.filter(a => a !== actor)
-        : [...prevData.actors, actor];
-      return { ...prevData, actors: actors.slice(0, 9) };
-    });
-  };
+  const filteredActors = options.actors.filter(actor =>
+    actor.actor_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-  const handleCheckboxChange = (item, field) => {
+  const handleCheckboxChange = (itemId, field) => {
     setFormData((prevData) => {
-      const selectedItems = prevData[field].includes(item)
-        ? prevData[field].filter(i => i !== item)
-        : [...prevData[field], item];
+      const selectedItems = prevData[field].includes(itemId)
+        ? prevData[field].filter(i => i !== itemId)
+        : [...prevData[field], itemId];
       return { ...prevData, [field]: selectedItems };
     });
   };
@@ -100,13 +95,6 @@ const InputMovieForm = () => {
     e.preventDefault();
 
     const movieData = new FormData();
-    // for (const key in formData) {
-    //   if (Array.isArray(formData[key])) {
-    //     movieData.append(key, formData[key].join(','));
-    //   } else {
-    //     movieData.append(key, formData[key]);
-    //   }
-    // }
 
     Object.keys(formData).forEach((key) => {
       if (Array.isArray(formData[key])) {
@@ -116,17 +104,10 @@ const InputMovieForm = () => {
       }
     });
 
-    // movieData.append('title', formData.title);
-    // movieData.append('alternative_title', formData.alternative_title);
-    // movieData.append('year', formData.year);
-    // movieData.append('country', formData.country);
-    // movieData.append('synopsis', formData.synopsis);
-    // movieData.append('platforms', formData.platforms.join(','));
-    // movieData.append('link_trailer', formData.link_trailer);
-    // movieData.append('award', formData.award);
-    // movieData.append('url_photo', formData.url_photo);
-    // movieData.append('genres', formData.genres.join(','));
-    // movieData.append('actors', formData.actors.join(','));
+    // Tambahkan log untuk memeriksa isi `movieData`
+    for (let pair of movieData.entries()) {
+      console.log(`${pair[0]}: ${pair[1]}`);
+    }
 
     try {
       MovieDataService.createMovie(movieData)
@@ -135,13 +116,13 @@ const InputMovieForm = () => {
           setFormData({
             title: '',
             year: '',
-            country: '',
+            country_id: '',
             synopsis: '',
             platforms: [],
             genres: [],
             actors: [],
             link_trailer: '',
-            award: '',
+            awardIds: '',
             url_photo: null
           });
         })
@@ -162,26 +143,47 @@ const InputMovieForm = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="space-y-2 justify-self-center">
-          <label htmlFor="upload-picture" className="block mb-2">Upload Poster</label>
+          <label htmlFor="upload-picture" className="block mb-2 font-bold">Upload Poster</label>
           <input type="file" className="border rounded p-2 h-72 w-30 bg-gray-700" id="upload-picture" onChange={handleFileChange} />
         </div>
 
         <div className="space-y-4 col-span-2">
           <div className="grid grid-cols-2 gap-4">
             <div>
-              <label htmlFor="title" className="block mb-2">Title</label>
-              <input type="text" id="title" className="w-full p-2 bg-gray-700 rounded" placeholder="Movie Title" value={formData.title} onChange={handleChange} />
+              <label htmlFor="title" className="block mb-2 font-bold">Title</label>
+              <input 
+                type="text" 
+                id="title" 
+                className="w-full p-2 bg-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500" 
+                placeholder="Movie Title" 
+                value={formData.title} 
+                onChange={handleChange} 
+                required
+              />
             </div>
           </div>
 
           <div className="grid grid-cols-2 gap-4">
             <div>
-              <label htmlFor="year" className="block mb-2">Year</label>
-              <input type="number" id="year" className="w-full p-2 bg-gray-700 rounded" placeholder="2024" value={formData.year} onChange={handleChange} />
+              <label htmlFor="year" className="block mb-2 font-bold">Year</label>
+              <input 
+                type="number" 
+                id="year" 
+                className="w-full p-2 bg-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500" 
+                placeholder="2024" 
+                value={formData.year} 
+                onChange={handleChange} 
+              />
             </div>
             <div>
-              <label htmlFor="country" className="block mb-2">Country</label>
-              <select id="country" className="w-full p-2 bg-gray-700 rounded" value={formData.country_id} onChange={handleChange}>
+              <label htmlFor="country_id" className="block mb-2 font-bold">Country</label>
+              <select 
+                id="country_id" 
+                className="w-full p-2 bg-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500" 
+                value={formData.country_id} 
+                onChange={handleChange}
+                required
+              >
                 <option value="">Select Country</option>
                 {options?.countries?.map((country) => (
                   <option key={country.country_id} value={country.country_id}>{country.country_name}</option>
@@ -191,23 +193,33 @@ const InputMovieForm = () => {
           </div>
 
           <div>
-            <label htmlFor="synopsis" className="block mb-2">Synopsis</label>
-            <textarea id="synopsis" rows="4" className="w-full p-2 bg-gray-700 rounded" placeholder="Synopsis" value={formData.synopsis} onChange={handleChange}></textarea>
+            <label htmlFor="synopsis" className="block mb-2 font-bold">Synopsis</label>
+            <textarea 
+              id="synopsis" 
+              rows="4" 
+              className="w-full p-2 bg-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500" 
+              placeholder="Synopsis" 
+              value={formData.synopsis} 
+              onChange={handleChange}
+            ></textarea>
           </div>
         </div>
       </div>
 
       <div className="space-y-4 mt-8">
         <div>
-          <label htmlFor="availability" className="block mb-2">Availability</label>
+          <label htmlFor="availability" className="block mb-2 font-bold">Availability</label>
           <div className="grid grid-cols-2 gap-2">
             {options?.platforms?.map(platform => (
-              <label key={platform.platform_id} value={platform.platform_id} className="flex items-center">
+              <label 
+                key={platform.platform_id} 
+                className="flex items-center cursor-pointer transition-transform transform hover:scale-105"
+              >
                 <input 
                   type="checkbox" 
-                  className="form-checkbox bg-gray-700 text-yellow-500" 
-                  onChange={() => handleCheckboxChange(platform.platform, 'platforms')} 
-                  checked={formData.platforms.includes(platform.platform)}
+                  className="form-checkbox bg-gray-700 text-yellow-500 h-5 w-5" 
+                  onChange={() => handleCheckboxChange(platform.platform_id, 'platforms')} 
+                  checked={formData.platforms.includes(platform.platform_id)}
                 />
                 <span className="ml-2">{platform.platform}</span>
               </label>
@@ -216,15 +228,19 @@ const InputMovieForm = () => {
         </div>
 
         <div className="grid grid-cols-4 gap-2">
-          <label className="block col-span-4">Genres</label>
+          <label className="block col-span-4 font-bold">Genres</label>
           <div className="grid grid-cols-2 gap-2">
             {options?.genres?.map((genre) => (
-              <label key={genre.genre_id} value={genre.genre_id} className="flex items-center">
+              <label 
+                key={genre.genre_id} 
+                value={genre.genre_id} 
+                className="flex items-center cursor-pointer transition-transform transform hover:scale-105"
+              >
                 <input 
                   type="checkbox" 
-                  className="form-checkbox bg-gray-700 text-yellow-500" 
-                  onChange={() => handleCheckboxChange(genre.genre, 'genres')}
-                  checked={formData.genres.includes(genre.genre)}
+                  className="form-checkbox bg-gray-700 text-yellow-500 h-5 w-5" 
+                  onChange={() => handleCheckboxChange(genre.genre_id, 'genres')}
+                  checked={formData.genres.includes(genre.genre_id)}
                 />
                 <span className="ml-2">{genre.genre}</span>
               </label>
@@ -232,33 +248,82 @@ const InputMovieForm = () => {
           </div>
         </div>
 
-        <div>
-          <label htmlFor="add_actor" className="block mb-2">Add Actor (Up to 9)</label>
-          <input type="text" id="add_actor" className="w-full p-2 bg-gray-700 rounded" placeholder="Search Actor Name" />
-          <div className="flex gap-4 mt-4 mb-4">
-            {options.actors.map(actor => (
-              <div key={actor.actor_id} className="text-center">
-                <img 
-                  src={`http://localhost:8080/uploads/actors/${actor.url_photo}`}
-                  alt={actor.actor_name}
-                  className="w-24 h-36 mx-auto rounded bg-gray-700 object-cover" 
-                  onClick={() => handleActorChange(actor.actor_name)}
-                />
-                <p className="mt-2">{actor.actor_name}</p>
-                {formData.actors.includes(actor.actor_name) && <span className="text-green-500">✓</span>}
+        <div className="space-y-4 mt-8">
+            {/* Search Actor Input */}
+            <div>
+              <label htmlFor="add_actor" className="block mb-2 font-bold">Add Actor (Up to 9)</label>
+              <input 
+                type="text" 
+                id="add_actor" 
+                className="w-full p-2 bg-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500" 
+                placeholder="Search Actor Name"
+                value={searchTerm}
+                onChange={handleSearchActorChange}
+              />
+            </div>
+
+            {/* Display Filtered Actor List */}
+            <div className="flex gap-4 mt-4 mb-4">
+              {filteredActors.map(actor => (
+                <div 
+                  key={actor.actor_id} 
+                  className={`text-center p-2 rounded-lg cursor-pointer transition-transform transform hover:scale-105 ${formData.actors.includes(actor.actor_id) ? 'border-2 border-yellow-500' : ''}`}
+                  onClick={() => handleActorChange(actor.actor_id)}
+                >
+                  <img 
+                    src={`http://localhost:8080/uploads/actors/${actor.url_photo}`}
+                    alt={actor.actor_name}
+                    className="w-24 h-36 mx-auto rounded bg-gray-700 object-cover"
+                    // onClick={() => handleActorChange(actor.actor_id)}
+                  />
+                  <p className="mt-2">{actor.actor_name}</p>
+                </div>
+              ))}
+            </div>
+
+            {/* Display Selected Actors */}
+            {formData.actors.length > 0 && (
+              <div>
+                <h3 className="text-white mb-2">Selected Actors:</h3>
+                <div className="flex gap-4">
+                  {formData.actors.map(actorId => {
+                    const actor = options.actors.find(a => a.actor_id === actorId);
+                    return actor ? (
+                      <div key={actor.actor_id} className="text-center">
+                        <img 
+                          src={`http://localhost:8080/uploads/actors/${actor.url_photo}`}
+                          alt={actor.actor_name}
+                          className="w-16 h-24 mx-auto rounded bg-gray-700 object-cover"
+                        />
+                        <p className="text-xs mt-2 text-white">{actor.actor_name}</p>
+                      </div>
+                    ) : null;
+                  })}
+                </div>
               </div>
-            ))}
-          </div>
+            )}
         </div>
 
         <div>
-          <label htmlFor="link_trailer" className="block mb-2">Link Trailer</label>
-          <input type="text" id="link_trailer" className="w-full p-2 bg-gray-700 rounded" placeholder="Link Trailer" onChange={handleChange} />
+          <label htmlFor="link_trailer" className="block mb-2 font-bold">Link Trailer</label>
+          <input 
+            type="text" 
+            id="link_trailer" 
+            className="w-full p-2 bg-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500" 
+            placeholder="Link Trailer" 
+            onChange={handleChange} 
+          />
         </div>
 
         <div>
-          <label htmlFor="award" className="block mb-2">Award</label>
-          <select id="award" className="w-full p-2 bg-gray-700 rounded" onChange={handleChange}>
+          <label htmlFor="awardIds" className="block mb-2 font-bold">Award</label>
+          <select 
+            id="awardIds" 
+            name="awardIds" 
+            className="w-full p-2 bg-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500" 
+            value={formData.awardIds} 
+            onChange={handleChange}
+          >
             <option value="">Select Award</option>
             {options.awards.map(award => (
               <option key={award.award_id} value={award.award_id}>{award.award_name}</option>
@@ -268,7 +333,11 @@ const InputMovieForm = () => {
       </div>
 
       <div className="mt-6 text-center">
-        <button className="bg-green-500 text-white text-xl font-bold py-2 px-4 rounded">Submit</button>
+        <button 
+          className="bg-green-500 hover:bg-green-600 text-white text-xl font-bold py-2 px-4 rounded transition-colors duration-300"
+        >
+          Submit
+        </button>
       </div>
     </form>
   );
