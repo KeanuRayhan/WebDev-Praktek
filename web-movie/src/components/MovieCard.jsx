83ec478843572061e737585123c47cb3a1d6b462
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-11-10 18:04:15 +0700

    Update MovieCard.jsx

--- web-movie/src/components/MovieCard.jsx
+++ web-movie/src/components/MovieCard.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const MovieCard = ({ title, url, year, genres, id }) => {
+  const [isInView, setIsInView] = useState(false);
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsInView(true);
+          observer.unobserve(cardRef.current);
+        }
+      },
+      { threshold: 0.25 }
+    );
+
+    if (cardRef.current) {
+      observer.observe(cardRef.current);
+    }
+
+    return () => {
+      if (cardRef.current) {
+        observer.unobserve(cardRef.current);
+      }
+    };
+  }, []);
+
   return (
     <Link to={`/movies/${id}`}>
-      <div className="bg-gray-800 border border-gray-700 shadow rounded-lg p-4 transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:border-yellow-500 hover:bg-gray-700 group">
+      <div 
+        ref={cardRef}
+        className="bg-gray-800 border border-gray-700 shadow rounded-lg p-4 transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:border-yellow-500 hover:bg-gray-700 group"
+      >
         <div className="relative overflow-hidden rounded-lg mb-2">
-          <img 
-            src={url} 
-            alt={title} 
-            className="w-full h-56 object-cover transition-transform duration-500 group-hover:scale-110" 
-          />
+          {isInView && (
+            <img 
+              src={`http://localhost:8080/${url}`}
+              alt={title} 
+              className="w-full h-56 object-cover transition-transform duration-500 group-hover:scale-110" 
+              loading='lazy'
+            />
+          )}
         </div>
         <h2 className="text-base leading-5 font-bold mb-2 text-white transition-colors duration-500 group-hover:text-yellow-400">
           {title}
